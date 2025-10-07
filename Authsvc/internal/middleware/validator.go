b059cccd94b52662ae33d474749798cb4781cec1
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-10-07 12:50:02 +0530

    Add a middleware function for validating incoming requests

---
+++ Authsvc/internal/middleware/validator.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"context"
+	"github.com/ganeshaditya1/GoBackend/Authsvc/internal/api"
+	"github.com/go-playground/validator/v10"
+	"net/http"	
+)
+
+var Validator = api.StrictMiddlewareFunc(func(handler api.StrictHandlerFunc, 
+											  endpoint string) api.StrictHandlerFunc{
+	curriedHandler := func(ctx context.Context, 
+					w http.ResponseWriter, 
+					r *http.Request, 
+					request interface{}) (interface{}, error) {
+				validate := validator.New(validator.WithRequiredStructEnabled())
+				errs := validate.Struct(request)
+				if errs != nil {
+					http.Error(w, errs.Error(), http.StatusBadRequest)
+					return nil, nil
+				}
+				return handler(ctx, w, r, request)
+	}
+	return api.StrictHandlerFunc(curriedHandler)
+})
