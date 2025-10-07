e1cf5ef341331b6a3a0d07ff4ddf2cd55849a41f
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-10-07 12:51:38 +0530

    Rename users.go file and add code for cosntructing models

--- Authsvc/internal/datalayer/models.go
+++ Authsvc/internal/datalayer/models.go
@@ -1,5 +1,11 @@
 package datalayer
 
+import (
+	"crypto/rand"
+	"crypto/sha256"
+    "encoding/hex"
+)
+
 type User struct {
     Userid int `db:"userid"`
     Username  string `db:"username"`
@@ -8,4 +14,44 @@ type User struct {
 	Salt string `db:"salt"`
 	Age int `db:"age"`
 	IsAdmin bool
+}
+
+func hashPassword(password string, salt string) string {
+    data := []byte(password + salt)
+
+    // Compute SHA-256 hash
+    hash := sha256.Sum256(data)
+
+    // Convert the hash to a hexadecimal string
+	return hex.EncodeToString(hash[:])
+}
+
+func generateSalt(length int) string {
+    // Create a byte slice to hold the random bytes
+    bytes := make([]byte, length)
+
+    // Read random bytes from the crypto/rand package
+    rand.Read(bytes)
+
+    // Convert bytes to a UTF-8 string
+    // Here we will limit the byte values to printable ASCII characters
+    for i := 0; i < length; i++ {
+        bytes[i] = byte(32 + (bytes[i] % 95)) // Printable ASCII range from 32 to 126
+    }
+
+    return string(bytes)
+}
+
+func NewUser(username string, email string, password string, age int) User {
+	// Create password salt.
+	password_salt := generateSalt(255)
+	password_hash := hashPassword(password, password_salt)
+
+	return User{
+		Username: username,
+		EmailAddress: email,
+		PasswordHash: password_hash,
+		Salt: password_salt,
+		Age: age,
+	}
 }
