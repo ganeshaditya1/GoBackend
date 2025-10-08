cc110a60470ba2e150ed3d87907476d43da0edb3
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-10-08 06:02:40 +0530

    Implement the bulk of the functionality of Datashardsvc

---
+++ Datasvc/cmd/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ganeshaditya1/GoBackend/Datasvc/internal/api"
+	. "github.com/ganeshaditya1/GoBackend/Datasvc/internal/config"
+	"github.com/ganeshaditya1/GoBackend/Datasvc/internal/datalayer"
+	. "github.com/ganeshaditya1/GoBackend/Datasvc/internal/logging"
+	"github.com/ganeshaditya1/GoBackend/Datasvc/internal/middleware"
+	"net/http"
+	"strings"
+)
+
+
+func start_server() {
+	// create a type that satisfies the `api.ServerInterface`, which contains an implementation of every operation from the generated code
+	server := api.NewServer()
+
+	r := http.NewServeMux()
+	
+	// get an `http.Handler` that we can use
+	allowed_origins := strings.Split(Config.SvcConfig.AllowedOrigins, ",")
+	cors := middleware.NewCORSFilter(allowed_origins)
+	ssi := api.NewStrictHandler(server, 
+							  []api.StrictMiddlewareFunc{middleware.Validator, cors})
+
+	// get an `http.Handler` that we can use
+	h := api.HandlerFromMux(ssi, r)
+
+
+	s := &http.Server{
+		Handler: h,
+		Addr:    fmt.Sprintf("0.0.0.0:%d", Config.SvcConfig.PortNo),
+	}
+
+	// And we serve HTTP until the world ends.
+	fmt.Println(s.ListenAndServe())
+}
+
+func main() {
+	InitConfig()
+	InitLogger()
+
+	dbusername := Config.DBConfig.Username
+	dbname := Config.DBConfig.DBname
+	dbportno := Config.DBConfig.Portno
+	datalayer.InitDB(dbname, dbusername, int(dbportno))
+
+
+	fmt.Println("Started Server")
+	start_server()
+
+
+	//err := datalayer.CreateUser("Hello", "Hello@gmail.com", "World", 69)
+	//fmt.Println(err)
+
+	//user, err := datalayer.LoginUser("Hello", "World")
+	//fmt.Println(user, err)
+}
---
+++ Datasvc/internal/api/handlers.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/ganeshaditya1/GoBackend/Datasvc/internal/datalayer"
+	"log/slog"
+)
+
+type Server struct {
+}
+
+func NewServer() Server {
+	return Server{}
+}
+
+func (serv Server) CreateBook(ctx context.Context, 
+						request CreateBookRequestObject) (CreateBookResponseObject, error) {
+    book := datalayer.NewBook(request.Body.BookName, request.Body.BookDescription)
+    err := datalayer.CreateBook(book)
+	if err != nil {
+		slog.Debug("Encountered an issue while inserting data into DB.", "DB error", err)
+		return nil, errors.New("Internal server error.")
+	}
+	return CreateBook201JSONResponse{}, nil
+}
+
+func (serv Server) RemoveBook(ctx context.Context, 
+	request RemoveBookRequestObject) (RemoveBookResponseObject, error) {
+	err := datalayer.RemoveBook(int(request.Id))
+	if err != nil {
+		slog.Error("Encountered an issue while removing record from DB.", "DB error", err)
+		return nil, errors.New("Internal server error.")
+	}
+	return RemoveBook204Response{}, nil
+}
+
+func (serv Server) SearchBooks(ctx context.Context, 
+	request SearchBooksRequestObject) (SearchBooksResponseObject, error) {
+
+	books, err := datalayer.SearchBooks(request.Params.Name)
+	if err != nil {
+		slog.Debug("Encountered an issue while reading from DB.", "DB error", err)
+		return nil, errors.New("Internal server error.")
+	}
+	bookCollection := make([]Book, 0, len(books))
+	for _, book := range books {
+		respBook := Book{
+			Bookid: int64(book.ID),
+			Bookname: book.Name,
+			Bookdescription: book.Description,
+		}
+		bookCollection = append(bookCollection, respBook)
+	}	
+	return SearchBooks200JSONResponse(BookCollection{
+		Items: &bookCollection,
+	}), nil
+}
