35d28ed47e37d943670c3e1dc43ee451d8d2da85
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-10-08 05:56:31 +0530

    Logging package for Datashardsvc

---
+++ Datasvc/internal/logging/logging.go
@@ -0,0 +1,20 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+	. "github.com/ganeshaditya1/GoBackend/Datasvc/internal/config"
+)
+
+func InitLogger() {
+	fhandle, err := os.OpenFile(Config.AppConfig.Logfile, 
+						   os.O_WRONLY | os.O_APPEND | os.O_CREATE,
+						0770)
+    if err != nil {
+        panic(err)
+    }
+	logger := slog.New(slog.NewJSONHandler(fhandle, nil))
+ 	slog.SetDefault(logger)
+
+	slog.Info("Logger initialized")
+}
